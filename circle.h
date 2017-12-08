112519f9bc480775235b05011d2ec5d35660065f
Maxwell Cui <cuizhaoyuan@email.arizona.edu>
2017-12-08 04:54:46 -0700

    first commit

---
+++ circle.h
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+
+
+void init(double *A, double *B, double *center, double *radius, double *tor)
+{
+  printf("\tThe default center is at (0,0), radius is 1, torlance is 0.000001. Do you want to change any of these values? (Y/N)");
+  
+  char sel;
+  scanf("%c",&sel);
+  /*
+  while(sel!='n' || sel!='N' || sel!='y' || sel!='Y')
+    {
+      printf("Invalid input, please enter again. (Y/N)");
+    }
+  */  
+  if(sel=='n' || sel=='N')
+    {
+      center[0]=0;
+      center[1]=0;
+      *radius=1;
+      *tor=0.000001;
+    }
+
+  else if(sel=='y' || sel=='Y')
+    {
+      printf("Please enter the value for x of center");
+      scanf("%lf",center[0]);
+      printf("Please enter the value for y of center");
+      scanf("%lf",center[1]);
+    }
+
+  printf("Please enter the value for x of A");
+  scanf("%lf",A[0]);
+  printf("Please enter the value for y of A");
+  scanf("%lf",A[1]);
+  
+  printf("Please enter the value for x of B");
+  scanf("%lf",B[0]);
+  printf("Please enter the value for y of B");
+  scanf("%lf",B[1]);
+}
+
+
+
+
+bool onArcCheck(double x, double y, double R, double tor)
+{
+  double D;
+  D=x*x+y*y;
+  if(fabs(sqrt(D)-R)<=tor)
+    return true;
+  else
+    return false;
+}
+
+void compare(double *A, double *B)
+{
+  if(A[0]==B[0] && A[1]==B[1])
+    {
+      printf("A and B are at the same location.\n");
+    }
+
+  else if(A[0]==-B[0] && A[1]==-B[1])
+    {
+      printf("A and B are symmetry respect to the center.\n");
+    }
+
+  else if(A[0]==0)
+    {
+      if(A[1]>0 && B[0]>0)
+	printf("The rotation is clockwise.\n");
+      
+      else if(A[1]>0 && B[0]<0)
+	printf("The rotation is counter clockwise.\n");
+      
+      else if(A[1]<0 && B[0]>0)
+	printf("The rotation is counter clockwise.\n");
+      
+      else if(A[1]<0 && B[0]<0)
+	printf("The rotation is  clockwise.\n");
+    }
+
+  else
+    {
+      double slope;
+      slope=A[1]/A[0];
+      if(A[0]>0)
+	{
+	  if(slope>0 && B[1]>slope*B[0])
+	    {
+	      printf("The rotation is counter clockwise.\n");
+	    }
+	  else if(slope>0 && B[1]<slope*B[0])
+	    {
+	      printf("The rotation is clockwise.\n");
+	    }
+	  else if(slope<0 && B[1]>slope*B[0])
+	    {
+	      printf("The rotation is counter clockwise.\n");
+	    }
+	  else if(slope<0 && B[1]<slope*B[0])
+	    {
+	      printf("The rotation is clockwise.\n");
+	    }
+	}
+      else if(A[0]<0)
+	{
+	  if(slope>0 && B[1]>slope*B[0])
+	    {
+	      printf("The rotation is clockwise.\n");
+	    }
+	  else if(slope>0 && B[1]<slope*B[0])
+	    {
+	      printf("The rotation is counter clockwise.\n");
+	    }
+	  else if(slope<0 && B[1]>slope*B[0])
+	    {
+	      printf("The rotation is clockwise.\n");
+	    }
+	  else if(slope<0 && B[1]<slope*B[0])
+	    {
+	      printf("The rotation is counter clockwise.\n");
+	    }
+	}
+    }
+}
